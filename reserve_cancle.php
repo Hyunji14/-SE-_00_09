79886e2e90d948b4db1c73c143de1b94386a65b9
Hyunji14 <blackit14@gmail.com>
2016-12-18 12:21:41 +0900

    Add files via upload

--- reserve_cancle.php
+++ reserve_cancle.php
@@ -7,25 +7,28 @@
 	$query = "select my_seat from list where id='$user_ID' and cash_code='$code'";
 	$result = mysqli_query($conn, $query);
 	$row = mysqli_fetch_row($result);
-	
-	$search_code_query = "select seat from bus_sc where code='$code'";
+
+	$qry = "select * from list where id='$user_ID' and cash_code='$code'";
+	$rr = mysqli_query($conn, $qry);
+	$rr = mysqli_fetch_row($rr);
+	$date = explode('-',$rr[4]);
+	$code = $rr[2].$rr[3].$date[1].$date[2].$rr[5].$rr[6];
+		
+	$search_code_query = "select * from bus_sc where code='$code'";
 	$search_result = mysqli_query($conn, $search_code_query);
 
 	$search_result = mysqli_fetch_row($search_result);
-	$array = unserialize($search_result[0]);
+
+	$array = unserialize($search_result[1]);
 
 	$seat_num = explode(',', $row[0]);
 	for($i=0; $i<count($seat_num); $i++)
 	{
 		$array[$seat_num[$i]-1] = 1;
-		print $seat_num[$i];
-		print "<br>";
-		print $array[$seat_num[$i]-1];
 	}
 	$serial_seat = serialize($array);
 	
 	$serial_update_query = "update bus_sc set seat='$serial_seat' where code='$code'";
-	print "<br>".$serial_update_query."<br>";
 	$update_result = mysqli_query($conn, $serial_update_query);
 
 	if($update_result){
@@ -33,11 +36,11 @@
 		$delete_result = mysqli_query($conn, $delete_query);
 		if($delete_result){
 			echo "<script>alert('Reserve Cancle is Success');</script>";
-			#echo "<script>location.href='home.php';</script>";
+			echo "<script>location.href='home.php';</script>";
 		}
 		else{
 			echo "<script>alert('Reserve Cancle is Failed.');</script>";
-			#echo "<script>location.href='reserveDetail.php';</script>";
+			echo "<script>location.href='reserveDetail.php';</script>";
 		}
 	}
 ?>
